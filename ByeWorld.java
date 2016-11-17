6190231869531825743e6c06ba5905799b6acef0
monsterbird13 <sam13.ballack@gmail.com>
2016-11-17 17:15:35 +0530

    byeworld created

---
+++ ByeWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Bye Bye, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
