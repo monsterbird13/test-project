53ba649b643980cbe52cc50da892488e7228f1b2
monsterbird13 <sam13.ballack@gmail.com>
2016-11-17 18:27:28 +0530

    delete

--- ByeWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        // Prints "Bye Bye, World" to the terminal window.
-        System.out.println("Hello, World");
-    }
-
-}
